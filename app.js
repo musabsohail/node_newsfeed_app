f01e626bce7f2301ea0b48ff288b8d438386007f
Musab Sohail <musabsohail93@gmail.com>
2021-12-14 05:15:17 +0500

    Add mongodb connection and mongoose

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL || "";
 
+// Connect to the database
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Connected to the database", DB_URL))
+  .catch(() => {
+    console.log("Failed to connect to the database");
+  });
+
+// Middlewares
 app.use(express.json());
+
+// API Routes
 app.use("/api", routes);
 
 app.use((req, res) => {
