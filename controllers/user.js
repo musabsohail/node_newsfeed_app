f823ecba423ecf119b7bfbe40fba8c08db031a4a
Musab Sohail <musabsohail93@gmail.com>
2021-12-14 05:16:29 +0500

    Add Users model

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,92 @@
+// User model
+const User = require("../models/user");
+
 const createUser = (req, res, next) => {
-  res.status(201).json({
-    username: "testuser",
+  const email = req.body.email || "";
+
+  User.isUserExists("email", email).then((isExists) => {
+    if (isExists) {
+      return res.status(501).json({
+        message: `User with email: ${email} already exists.`,
+      });
+    }
+  });
+
+  User.create(req.body).then((doc) => {
+    return res.status(201).json({
+      data: [
+        {
+          ...doc._doc,
+        },
+      ],
+    });
   });
 };
 
 const getUsers = (req, res, next) => {
-  res.json({
-    data: [{ username: "testuser" }, { username: "test2" }],
+  User.find().then((users) => {
+    res.status(200).json({
+      data: [...users],
+    });
   });
 };
 
 const getUserById = (req, res, next) => {
-  res.json({
-    username: req.params.userId,
-  });
+  User.findById(req.params.userId)
+    .then((doc) => {
+      res.json({
+        data: [
+          {
+            ...doc._doc,
+          },
+        ],
+      });
+    })
+    .catch(({ message }) => {
+      return res.status(400).json({
+        message,
+      });
+    });
 };
 
 const updateUser = (req, res, next) => {
-  res.json({
-    username: req.params.userId,
+  const email = req.body.email || "";
+  const userId = req.params.userId || "";
+  User.isUserExists("email", email).then((isExists) => {
+    if (isExists) {
+      return res.status(501).json({
+        message: `User with email: ${email} already exists.`,
+      });
+    }
   });
+
+  User.findOneAndUpdate({ _id: userId }, { ...req.body })
+    .then((doc) => {
+      return res.json({
+        data: [
+          {
+            ...doc._doc,
+          },
+        ],
+      });
+    })
+    .catch(({ message }) => {
+      return res.status(500).json({
+        message,
+      });
+    });
 };
+
 const deleteUser = (req, res, next) => {
-    res.status(204);
+  User.deleteOne({ _id: req.params.userId })
+    .then(() => {
+      return res.status(204).json({});
+    })
+    .catch(({ message }) => {
+      return res.status(400).json({
+        message,
+      });
+    });
 };
 
 module.exports = {
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  username: String,
+  email: {
+    type: String,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email");
+      }
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.statics.isUserExists = async function (findByKey, value) {
+  const user = await this.findOne({ [findByKey]: value });
+  return !!user;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
